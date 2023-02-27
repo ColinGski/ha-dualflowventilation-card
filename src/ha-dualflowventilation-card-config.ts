68198dd90e81cdc5160312f8ecd73c0b420a18a2
NoOne <colin.gebski@gmail.com>
2023-02-27 17:21:08 +0100

    initial commit

---
+++ src/ha-dualflowventilation-card-config.ts
@@ -0,0 +1,11 @@
+export interface DualFlowVentilationCardConfig
+{
+    exhaust_air_entity: any;
+    outdoor_air_entity: any;
+    fan_speed_entity: any;
+    humidity_entity: any;
+    cell_state_entity: any;
+    efficiency_entity: any;
+    supply_air_entity: any;
+    extract_air_entity: any;
+};
